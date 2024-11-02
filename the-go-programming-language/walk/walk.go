7727d48b26172a5d8ffe68486212d3a41e6149cb
Dorde Kesic <dordekesic.keso@gmail.com>
2024-11-02 14:50:03 +0100

    adds some reflection type tests

--- the-go-programming-language/walk/walk.go
+++ the-go-programming-language/walk/walk.go
@@ -2,17 +2,56 @@ package walk
 
 import "reflect"
 
-func Walk(r any, fn func(string, string)) {
+func Walk(r any, fn func(string, string), namespace string) {
 	rValue := reflect.ValueOf(r)
+
+	if rValue.Kind() == reflect.Pointer {
+		rValue = rValue.Elem()
+		r = rValue.Interface()
+	}
+
 	if rValue.Kind() != reflect.Struct {
 		return
 	}
 
 	for i := 0; i < rValue.NumField(); i++ {
 		fValue := rValue.Field(i)
+		fName := reflect.TypeOf(r).Field(i).Name
 		if fValue.Kind() != reflect.String {
+			Walk(fValue.Interface(), fn, fName)
 			continue
 		}
-		fn(reflect.TypeOf(r).Field(i).Name, fValue.String())
+		if len(namespace) > 0 {
+			fName = namespace + "." + fName
+		}
+		fn(fName, fValue.String())
 	}
+}
+
+func WalkAlt(r any, fn func(string)) {
+	rValue := getValue(r)
+
+	switch rValue.Kind() {
+	case reflect.Struct:
+		for i := 0; i < rValue.NumField(); i++ {
+			f := rValue.Field(i)
+			WalkAlt(f.Interface(), fn)
+		}
+	case reflect.Chan:
+		for v, ok := rValue.Recv(); ok; v, ok = rValue.Recv() {
+			WalkAlt(v.Interface(), fn)
+		}
+	case reflect.String:
+		fn(rValue.String())
+	}
+}
+
+func getValue(r any) reflect.Value {
+	rValue := reflect.ValueOf(r)
+
+	if rValue.Kind() == reflect.Pointer {
+		rValue = rValue.Elem()
+	}
+
+	return rValue
 }
--- the-go-programming-language/walk/walk_test.go
+++ the-go-programming-language/walk/walk_test.go
@@ -6,6 +6,75 @@ import (
 	"walk"
 )
 
+func TestWalkAlt(t *testing.T) {
+	t.Run("simple cases", func(t *testing.T) {
+		testCases := []struct {
+			name  string
+			input any
+			want  []string
+		}{
+			{"simple string",
+				"just testing",
+				[]string{"just testing"},
+			},
+			{
+				"single string struct",
+				struct{ Value string }{"some string"},
+				[]string{"some string"},
+			},
+			{
+				"pointer struct",
+				struct {
+					Value *struct {
+						InnerValue   string
+						AnotherValue string
+					}
+				}{
+					&struct {
+						InnerValue   string
+						AnotherValue string
+					}{"inner value", "another value"},
+				},
+				[]string{"inner value", "another value"},
+			},
+		}
+
+		for _, tc := range testCases {
+			t.Run(tc.name, func(t *testing.T) {
+				got := make([]string, 0, 1)
+				walk.WalkAlt(tc.input, func(v string) {
+					got = append(got, v)
+				})
+				if !reflect.DeepEqual(got, tc.want) {
+					t.Errorf("slices mismatch, got %v, want %v", got, tc.want)
+				}
+			})
+		}
+	})
+
+	t.Run("channels test", func(t *testing.T) {
+		ch := make(chan string)
+
+		want := []string{"value1", "value2"}
+		got := make([]string, 0, 2)
+
+		go func() {
+			for _, v := range want {
+				ch <- v
+			}
+			close(ch)
+		}()
+
+		walk.WalkAlt(ch, func(v string) {
+			got = append(got, v)
+		})
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("chan result mismatch, want %v, got %v", want, got)
+		}
+	})
+}
+
 func TestWalk(t *testing.T) {
 	t.Run("returns slice of string field names", func(t *testing.T) {
 		obj := struct {
@@ -13,7 +82,19 @@ func TestWalk(t *testing.T) {
 			Surname  string
 			Age      int
 			Nickname string
-		}{"name", "surname", 5, "nickname"}
+			Profile  struct {
+				Address string
+				Count   int
+			}
+			Alt *struct {
+				Field string
+			}
+		}{"name", "surname", 5, "nickname", struct {
+			Address string
+			Count   int
+		}{"address", 5}, &struct {
+			Field string
+		}{"alt"}}
 
 		type result struct {
 			name  string
@@ -24,12 +105,14 @@ func TestWalk(t *testing.T) {
 			{"Name", "name"},
 			{"Surname", "surname"},
 			{"Nickname", "nickname"},
+			{"Profile.Address", "address"},
+			{"Alt.Field", "alt"},
 		}
 
 		got := make([]result, 0, 3)
 		walk.Walk(obj, func(name string, value string) {
 			got = append(got, result{name, value})
-		})
+		}, "")
 
 		if !reflect.DeepEqual(want, got) {
 			t.Errorf("want %v, got %v", want, got)
@@ -39,6 +122,6 @@ func TestWalk(t *testing.T) {
 	t.Run("returns empty slice for non struct variable", func(t *testing.T) {
 		walk.Walk(5, func(name string, value string) {
 			t.Errorf("function should not be called, received name %s, value %s", name, value)
-		})
+		}, "")
 	})
 }
